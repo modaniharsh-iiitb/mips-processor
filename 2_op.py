72942cf4207467eb5b3ec8c086b8e7a270492b60
Owais-Md <mohammad.owais@iiitb.ac.in>
2023-11-09 13:18:28 +0530

    looks like it works

--- 2_op.py
+++ 2_op.py
@@ -13,16 +13,16 @@ clock = clk(0.25)
 # queue for instructions(with max length 5)
 
 # IF/ID pipeline register
-IFIDReg = {'valid' : 0, 'stall' : 0} # has only instr
+IFIDReg = {'valid' : 0, 'stall' : 0, 'instr' : 0} # has only instr
 
 # ID/EX pipeline register
-IDEXReg = {'valid' : 0, 'stall' : 0} # has cRegDst, cAluSrc, cMemReg, cRegWr, cMemRd, cMemWr, cBranch, cAluOp, cHiLoWr, cLoRd, cHiRd, cJmp, cLink, cJr, jTarget, rdData1, rdData2, immed, opcode, func, wReg
+IDEXReg = {'valid' : 0, 'stall' : 0, 'instr' : 0} # has cRegDst, cAluSrc, cMemReg, cRegWr, cMemRd, cMemWr, cBranch, cAluOp, cHiLoWr, cLoRd, cHiRd, cJmp, cLink, cJr, jTarget, rdData1, rdData2, immed, opcode, func, wReg
 
 # EX/MEM pipeline register
-EXMEMReg = {'valid' : 0, 'stall' : 0}
+EXMEMReg = {'valid' : 0, 'stall' : 0, 'instr' : 0}
 
 # MEM/WB pipeline register
-MEMWBReg = {'valid' : 0, 'stall' : 0}
+MEMWBReg = {'valid' : 0, 'stall' : 0, 'instr' : 0}
 
 class instrBuffer:
     def __init__(self):
@@ -35,12 +35,16 @@ class instrBuffer:
         # put fetch result in IFIDReg
         content = [instr]
         IFIDReg.update({'valid' : 1, 'content' : content})
+        print("IF:")
+        print(bin(IFIDReg['content'][0])[2:].zfill(32))
+        IFIDReg.update({'instr' : (getPC()-int(0x400000))//4})
+        print(IFIDReg)
         return instr
     
     def bufferDecode(self):
         # decode on instr in IFIDReg
         if IFIDReg['valid']:
-            instr, = IFIDReg['content']
+            instr, = IFIDReg['content'] # unpack instr
             if instr != 0:
                 # stage: instruction decode
                 # control signals
@@ -49,6 +53,9 @@ class instrBuffer:
                 # decoded registers and data
                 decodeOutput = list(decode(instr, cRegDst, cLoRd, cHiRd, cJmp, cJr, cLink))
                 IDEXReg.update({'valid' : 1, 'controlUnitOutput' : controlUnitOutput, 'decodeOutput': decodeOutput})
+                print('ID:')
+                IDEXReg.update({'instr' : IFIDReg['instr']})
+                print(IDEXReg)
             # if decodeOutput[4] == lw ka opcode:
             # 
         
@@ -60,6 +67,9 @@ class instrBuffer:
             pcTemp, rdData2, aluRes1, aluRes2, wReg = execute(pcTemp, rdData1, rdData2, immed, opcode, func, wReg, cAluOp, cAluSrc, cBranch, cLoRd, cHiRd)
             executeOutput = list([pcTemp, rdData2, aluRes1, aluRes2, wReg])
             EXMEMReg.update({'valid' : 1, 'controlUnitOutput' : IDEXReg['controlUnitOutput'], 'executeOutput' : executeOutput})
+            print('EX:')
+            EXMEMReg.update({'instr' : IDEXReg['instr']})
+            print(EXMEMReg)
 
     def bufferMemory(self):
         # memory stage on instr in EXMEMReg
@@ -69,6 +79,9 @@ class instrBuffer:
             wData, aluRes1, aluRes2, wReg = memory(pcTemp, rdData2, aluRes1, aluRes2, wReg, cMemWr, cMemRd, cMemReg, cLink)
             memoryOutput = list([wData, aluRes1, aluRes2, wReg])
             MEMWBReg.update({'valid' : 1, 'controlUnitOutput' : EXMEMReg['controlUnitOutput'], 'memoryOutput' : memoryOutput})
+            print('MEM:')
+            MEMWBReg.update({'instr' : EXMEMReg['instr']})
+            print(MEMWBReg)
 
     def bufferWriteback(self):
         # writeback stage on instr in MEMWBReg
@@ -76,26 +89,30 @@ class instrBuffer:
             cRegDst, cAluSrc, cMemReg, cRegWr, cMemRd, cMemWr, cBranch, cAluOp, cHiLoWr, cLoRd, cHiRd, cJmp, cLink, cJr = MEMWBReg['controlUnitOutput']
             wData, aluRes1, aluRes2, wReg = MEMWBReg['memoryOutput']
             writeback(wData, aluRes1, aluRes2, wReg, cRegWr, cHiLoWr)
+            print('WB')
+            print(MEMWBReg)
 
 IB = instrBuffer()
 
 while True:
-    # IF
-    if (IB.bufferFetch() == 0):
-        break
-    
-    # ID
-    IB.bufferDecode()
-
-    # EX
-    IB.bufferExecute()
+    # WB
+    IB.bufferWriteback()
 
     # MEM
     IB.bufferMemory()
 
-    # WB
-    IB.bufferWriteback()
+    # EX
+    IB.bufferExecute()
+    
+    # ID
+    IB.bufferDecode()
+
+    # IF
+    if (IB.bufferFetch() == 0):
+        break
 
     clock.cycle()
+    print()
+    print()
 
 print('No. of cycles:', clock.noOfCycles())
